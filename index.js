6d59fe3322c6dde6b358e3c396f7d4825d8c256b
DylanJaundoo <117795874+DylanJaundoo@users.noreply.github.com>
2023-08-05 00:06:58 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function github(){
+    window.location='https://github.com/DylanJaundoo'
+}
+
+function twitter(){
+    window.location='https://twitter.com/djaundoo_codes'
+}
+
+function linkedin(){
+    window.location='https://www.linkedin.com/in/dylan-jaundoo-822b68212/'
+}
